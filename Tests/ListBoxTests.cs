ffb95e1e7ceee56bd0f77412a297280e93732184
Mathew Glodack <mathewg@procore.com>
2019-02-26 13:43:25 -0800

    chore: Add skeleton for ListBox UI tests

---
+++ Tests/ListBoxTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class ListBoxTests : WpfAppSession
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+    }
+}
